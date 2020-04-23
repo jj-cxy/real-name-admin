f26ef9249143c34bf02f8426f2d62a1c30332aef
594yxy <1354775931@qq.com>
2020-04-23 10:36:03 +0800

    设置修改

--- src/components/SettingDrawer/settingConfig.js
+++ src/components/SettingDrawer/settingConfig.js
@@ -1,32 +1,41 @@
-import { message } from 'ant-design-vue/es'
+import {
+  message
+} from 'ant-design-vue/es'
 // import defaultSettings from '../defaultSettings';
 
 let lessNodesAppended
 
-const colorList = [
-  {
-    key: '薄暮', color: '#F5222D'
+const colorList = [{
+    key: '薄暮',
+    color: '#F5222D'
   },
   {
-    key: '火山', color: '#FA541C'
+    key: '火山',
+    color: '#FA541C'
   },
   {
-    key: '日暮', color: '#FAAD14'
+    key: '日暮',
+    color: '#FAAD14'
   },
   {
-    key: '明青', color: '#13C2C2'
+    key: '明青',
+    color: '#13C2C2'
   },
   {
-    key: '极光绿', color: '#52C41A'
+    key: '极光绿',
+    color: '#52C41A'
   },
   {
-    key: '拂晓蓝（默认）', color: '#1890FF'
+    key: '拂晓蓝（默认）',
+    color: '#1890FF'
   },
   {
-    key: '极客蓝', color: '#2F54EB'
+    key: '极客蓝',
+    color: '#2F54EB'
   },
   {
-    key: '酱紫', color: '#722ED1'
+    key: '酱紫',
+    color: '#722ED1'
   }
 ]
 
@@ -40,6 +49,7 @@ const updateTheme = primaryColor => {
     return
   }
   const hideMessage = message.loading('正在编译主题！', 0)
+
   function buildIt() {
     if (!window.less) {
       return
@@ -92,4 +102,8 @@ const updateColorWeak = colorWeak => {
   colorWeak ? document.body.classList.add('colorWeak') : document.body.classList.remove('colorWeak')
 }
 
-export { updateTheme, colorList, updateColorWeak }
+export {
+  updateTheme,
+  colorList,
+  updateColorWeak
+}
